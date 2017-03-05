fb5628edb43d7f811d822c93515e13cbfd9392f0
Yucheng Tu <yucheng.tu@qianmo-inc.com>
2017-03-05 23:24:33 +0800

    update language

--- webpack.config.js
+++ webpack.config.js
@@ -1,147 +1,3 @@
-// const {resolve, join} = require('path');
-// const webpack = require('webpack');
-
-
-// const sourcePath = join(__dirname, './client');
-// const staticsPath = join(__dirname, './static');
-// module.exports = function (env) {
-//   const nodeEnv = env && env.prod ? 'production' : 'development';
-//   const isProd = nodeEnv === 'production';
-
-//   const plugins = [
-//     new webpack.optimize.CommonsChunkPlugin({
-//       name: 'vendor',
-//       minChunks: Infinity,
-//       filename: 'vendor.bundle.js'
-//     }),
-//     new webpack.EnvironmentPlugin({
-//       NODE_ENV: nodeEnv,
-//     }),
-//     new webpack.NamedModulesPlugin(),
-//   ];
-
-//   if (isProd) {
-//     plugins.push(
-//       new webpack.LoaderOptionsPlugin({
-//         minimize: true,
-//         debug: false
-//       }),
-//       new webpack.optimize.UglifyJsPlugin({
-//         compress: {
-//           warnings: false,
-//           screw_ie8: true,
-//           conditionals: true,
-//           unused: true,
-//           comparisons: true,
-//           sequences: true,
-//           dead_code: true,
-//           evaluate: true,
-//           if_return: true,
-//           join_vars: true,
-//         },
-//         output: {
-//           comments: false,
-//         },
-//       })
-//     );
-//   } else {
-//     plugins.push(
-//       new webpack.HotModuleReplacementPlugin()
-//     );
-//   }
-
-//   return {
-//     devtool: isProd ? 'source-map' : 'eval',
-//     context: sourcePath,
-//     entry: {
-//       js: './index.js',
-//       vendor: [
-//         'react',
-//         // 需要的库
-//       ]
-//     },
-//     output: {
-//       path: staticsPath,
-//       filename: '[name].bundle.js',
-//     },
-//     module: {
-//       rules: [
-//         {
-//           test: /\.html$/,
-//           exclude: /node_modules/,
-//           use: {
-//             loader: 'file-loader',
-//             query: {
-//               name: '[name].[ext]'
-//             },
-//           },
-//         },
-//         {
-//           test: /\.css$/,
-//           exclude: /node_modules/,
-//           use: [
-//             'style-loader',
-//             'css-loader'
-//           ]
-//         },
-//         {
-//           test: /\.(js|jsx)$/,
-//           exclude: /node_modules/,
-//           use: [
-//             'babel-loader'
-//           ],
-//         },
-//       ],
-//     },
-//     resolve: {
-//       extensions: ['.webpack-loader.js', '.web-loader.js', '.loader.js', '.js', '.jsx'],
-//       modules: [
-//         resolve(__dirname, 'node_modules'),
-//         sourcePath
-//       ]
-//     },
-
-//     plugins,
-
-//     performance: isProd && {
-//       maxAssetSize: 100,
-//       maxEntrypointSize: 300,
-//       hints: 'warning',
-//     },
-
-//     stats: {
-//       colors: {
-//         green: '\u001b[32m',
-//       }
-//     },
-
-//     devServer: {
-//       contentBase: './client',
-//       historyApiFallback: true,
-//       port: 9424,
-//       compress: isProd,
-//       inline: !isProd,
-//       hot: !isProd,
-//       stats: {
-//         assets: true,
-//         children: false,
-//         chunks: false,
-//         hash: false,
-//         modules: false,
-//         publicPath: false,
-//         timings: true,
-//         version: false,
-//         warnings: true,
-//         colors: {
-//           green: '\u001b[32m',
-//         }
-//       },
-//     }
-//   };
-// };
-
-
-
 const {join, resolve} = require('path');
 const HtmlwebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
@@ -170,7 +26,6 @@ module.exports = {
         test: /\.less$/i,
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
-          // use: ['css-loader', 'less-loader']
           use: [
             {
               loader: 'css-loader',
@@ -198,9 +53,10 @@ module.exports = {
       {
         test: /\.(ico|png|jpg|svg)$/,
         loader: 'url-loader',
+        include: /client\/images/,
         options: {
-          limit: 1024,
-          name: '/images/[name].[hash:base64:5]'
+          limit: 10240,
+          name: 'images/[name].[hash:base64:5]'
         }
       },
       {
