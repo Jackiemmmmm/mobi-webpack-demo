d4c56c74591c84107343cafd3f129f6683219f28
Jackiemmmmm <jackie.tu@btcc.com>
2017-03-06 15:02:27 +0800

    add svg and remove react intl

---
+++ client/icons/ArrowLeftIcon.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ArrowLeftIcon = ({ color = '#FFF' }) => (
+  <svg viewBox="0 0 18 18" height="1.1rem" width="1.1rem" fill={color}>
+    <path d="M13.703 16.293a1 1 0 1 1-1.415 1.414l-7.995-8a1 1 0 0 1 0-1.414l7.995-8a1 1 0 1 1 1.415 1.414L6.413 9l7.29 7.293z" />
+  </svg>
+)
+
+export default ArrowLeftIcon
